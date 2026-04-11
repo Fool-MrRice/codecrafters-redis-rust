c1062c6ec6303c4e90127d80a3c310761c99c77f
Fool-MrRice <162326010+Fool-MrRice@users.noreply.github.com>
2026-04-11 14:51:05 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,19 @@ fn main() {
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     let mut buf = [0u8; 1024];
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let n = stream.read(&mut buf).unwrap();
-                if n != 0 {
-                    stream.write_all(b"+PONG\r\n").unwrap();
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    println!("accepted new connection");
+                    let n = stream.read(&mut buf).unwrap();
+                    if n != 0 {
+                        stream.write_all(b"+PONG\r\n").unwrap();
+                    }
                 }
-            }
-            Err(e) => {
-                println!("error: {}", e);
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
